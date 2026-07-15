d921af180356c4942fca5d17676aa3420c33bdc4
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-07-15 18:35:21 +0800

    3_userinput.py

---
+++ 3_userinput.py
@@ -0,0 +1,9 @@
+age = float(input("What is your age?:")) # 13 #always returns your response in a string 13 = "37"
+
+# control statement - conditional statements
+
+if age>= 18:
+    print("You are an adult")
+
+if age < 18:
+    print("You are a kid")
