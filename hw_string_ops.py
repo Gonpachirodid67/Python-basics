2b2d5a09876d1e431c8dde013e1e99d65c0dc5c8
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-07-17 15:22:37 +0800

    hw_string_ops.py

---
+++ hw_string_ops.py
@@ -0,0 +1,15 @@
+goat = "Messi is better than Ronaldo."
+print(goat)
+
+upperStr = goat.upper()
+print(upperStr)
+
+lowerStr = goat.lower()
+print(lowerStr)
+
+print(len(goat))
+
+print(goat[0])
+
+thebest = goat.replace("better than Ronaldo.", "the best in the world.")
+print(thebest)
