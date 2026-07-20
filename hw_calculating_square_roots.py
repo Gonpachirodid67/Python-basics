66d302233f74a7a3695faa67e6bfa538a0104e06
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-07-20 17:01:36 +0800

    hw_calculating_square_roots.py

---
+++ hw_calculating_square_roots.py
@@ -0,0 +1,303 @@
+print("Calculating Square Roots")
+
+square = float(input("Which number's square root do you want to know?:"))
+
+if square == 1:
+    print(1**2)
+
+if square == 2:
+    print(2**2)
+
+if square == 3:
+    print(3**2)
+
+if square == 4:
+    print(4**2)
+
+if square == 5:
+    print(5**2)
+
+if square == 6:
+    print(6**2)
+
+if square == 7:
+    print(7**2)
+
+if square == 8:
+    print(8**2)
+
+if square == 9:
+    print(9**2)
+
+if square == 10:
+    print(10**2)
+
+if square == 11:
+    print(11**2)
+
+if square == 12:
+    print(12**2)
+
+if square == 13:
+    print(13**2)
+
+if square == 14:
+    print(14**2)
+
+if square == 15:
+    print(15**2)
+
+if square == 16:
+    print(16**2)
+
+if square == 17:
+    print(17**2)
+
+if square == 18:
+    print(18**2)
+
+if square == 19:
+    print(19**2)
+
+if square == 20:
+    print(20**2)
+
+if square == 21:
+    print(21**2)
+
+if square == 22:
+    print(22**2)
+
+if square == 23:
+    print(23**2)
+
+if square == 24:
+    print(24**2)
+
+if square == 25:
+    print(25**2)
+
+if square == 26:
+    print(26**2)
+
+if square == 27:
+    print(27**2)
+
+if square == 28:
+    print(28**2)
+
+if square == 29:
+    print(29**2)
+    
+if square == 30:
+    print(30**2)
+
+if square == 31:
+    print(31**2)
+
+if square == 32:
+    print(32**2)
+
+if square == 33:
+    print(33**2)
+
+if square == 34:
+    print(34**2)
+
+if square == 35:
+    print(35**2)
+
+if square == 36:
+    print(36**2)
+
+if square == 37:
+    print(37**2)
+
+if square == 38:
+    print(38**2)
+
+if square == 39:
+    print(39**2)
+
+if square == 40:
+    print(40**2)
+
+if square == 41:
+    print(41**2)
+
+if square == 42:
+    print(42**2)
+
+if square == 43:
+    print(43**2)
+
+if square == 44:
+    print(44**2)
+
+if square == 45:
+    print(45**2)
+
+if square == 46:
+    print(46**2)
+
+if square == 47:
+    print(47**2)
+
+if square == 48:
+    print(48**2)
+
+if square == 49:
+    print(49**2)
+
+if square == 50:
+    print(50**2)
+
+if square == 51:
+    print(51**2)
+
+if square == 52:
+    print(52**2)
+
+if square == 53:
+    print(53**2)
+
+if square == 54:
+    print(54**2)
+
+if square == 55:
+    print(55**2)
+
+if square == 56:
+    print(56**2)
+
+if square == 57:
+    print(57**2)
+
+if square == 58:
+    print(58**2)
+
+if square == 59:
+    print(59**2)
+
+if square == 60:
+    print(60**2)
+
+if square == 61:
+    print(61**2)
+
+if square == 62:
+    print(62**2)
+
+if square == 63:
+    print(63**2)
+
+if square == 64:
+    print(64**2)
+
+if square == 65:
+    print(65**2)
+
+if square == 66:
+    print(66**2)
+
+if square == 67:
+    print(67**2)
+
+if square == 68:
+    print(68**2)
+
+if square == 69:
+    print(69**2)
+
+if square == 70:
+    print(70**2)
+
+if square == 71:
+    print(71**2)
+
+if square == 72:
+    print(72**2)
+
+if square == 73:
+    print(73**2)
+
+if square == 74:
+    print(74**2)
+
+if square == 75:
+    print(75**2)
+
+if square == 76:
+    print(76**2)
+
+if square == 77:
+    print(77**2)
+
+if square == 78:
+    print(78**2)
+
+if square == 79:
+    print(79**2)
+
+if square == 80:
+    print(80**2)
+
+if square == 81:
+    print(81**2)
+
+if square == 82:
+    print(82**2)
+
+if square == 83:
+    print(83**2)
+
+if square == 84:
+    print(84**2)
+
+if square == 85:
+    print(85**2)
+
+if square == 86:
+    print(86**2)
+
+if square == 87:
+    print(87**2)
+
+if square == 88:
+    print(88**2)
+
+if square == 89:
+    print(89**2)
+
+if square == 90:
+    print(90**2)
+
+if square == 91:
+    print(91**2)
+
+if square == 92:
+    print(92**2)
+
+if square == 93:
+    print(93**2)
+
+if square == 94:
+    print(94**2)
+
+if square == 95:
+    print(95**2)
+
+if square == 96:
+    print(96**2)
+
+if square == 97:
+    print(97**2)
+
+if square == 98:
+    print(98**2)
+
+if square == 99:
+    print(99**2)
+
+if square == 100:
+    print(100**2)
