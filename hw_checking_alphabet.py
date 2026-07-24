61a2bc8c910dcf9f45edd5c0f14d63cb2ab3548a
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-07-24 17:44:15 +0800

    hw_checking_alphabet.py

--- hw_checking_alphabet.py
+++ hw_checking_alphabet.py
@@ -2,57 +2,60 @@ print("Let's play - Is it a Alphabet or not")
 print("Only enter 1 word")
 alphabet = input("Let me guess if it is a Alphabet or not?:")
 
-if alphabet == "a" or "A":
-    print("It is an Alphabet")
-elif alphabet == "b" or "B":
-    print("It is an Alphabet")
-elif alphabet == "c" or "C":
-    print("It is an Alphabet")
-elif alphabet == "d" or "D":
-    print("It is an Alphabet")
-elif alphabet == "e" or "E":
-    print("It is an Alphabet")
-elif alphabet == "f" or "F":
-    print("It is an Alphabet")
-elif alphabet == "g" or "G":
-    print("It is an Alphabet")
-elif alphabet == "h" or "H":
-    print("It is an Alphabet")
-elif alphabet == "i" or "I":
-    print("It is an Alphabet")
-elif alphabet == "j" or "J":
-    print("It is an Alphabet")
-elif alphabet == "k" or "K":
-    print("It is an Alphabet")
-elif alphabet == "l" or "L":
-    print("It is an Alphabet")
-elif alphabet == "m" or "M":
-    print("It is an Alphabet")
-elif alphabet == "n" or "N":
-    print("It is an Alphabet")
-elif alphabet == "o" or "O":
-    print("It is an Alphabet")
-elif alphabet == "p" or "P":
-    print("It is an Alphabet")
-elif alphabet == "q" or "Q":
-    print("It is an Alphabet")
-elif alphabet == "r" or "R":
-    print("It is an Alphabet")
-elif alphabet == "s" or "S":
-    print("It is an Alphabet")
-elif alphabet == "t" or "T":
-    print("It is an Alphabet")
-elif alphabet == "u" or "U":
-    print("It is an Alphabet")
-elif alphabet == "v" or "V":
-    print("It is an Alphabet")
-elif alphabet == "w" or "W":
-    print("It is an Alphabet")
-elif alphabet == "x" or "X":
-    print("It is an Alphabet")
-elif alphabet == "y" or "Y":
-    print("It is an Alphabet")
-elif alphabet == "z" or "Z":
-    print("It is an Alphabet")
+if isinstance(alphabet, (int, float)) and not isinstance(alphabet, bool):
+    if alphabet == "a" or "A":
+        print("It is an Alphabet")
+    elif alphabet == "b" or "B":
+        print("It is an Alphabet")
+    elif alphabet == "c" or "C":
+        print("It is an Alphabet")
+    elif alphabet == "d" or "D":
+        print("It is an Alphabet")
+    elif alphabet == "e" or "E":
+        print("It is an Alphabet")
+    elif alphabet == "f" or "F":
+        print("It is an Alphabet")
+    elif alphabet == "g" or "G":
+        print("It is an Alphabet")
+    elif alphabet == "h" or "H":
+        print("It is an Alphabet")
+    elif alphabet == "i" or "I":
+        print("It is an Alphabet")
+    elif alphabet == "j" or "J":
+        print("It is an Alphabet")
+    elif alphabet == "k" or "K":
+        print("It is an Alphabet")
+    elif alphabet == "l" or "L":
+        print("It is an Alphabet")
+    elif alphabet == "m" or "M":
+        print("It is an Alphabet")
+    elif alphabet == "n" or "N":
+        print("It is an Alphabet")
+    elif alphabet == "o" or "O":
+        print("It is an Alphabet")
+    elif alphabet == "p" or "P":
+        print("It is an Alphabet")
+    elif alphabet == "q" or "Q":
+        print("It is an Alphabet")
+    elif alphabet == "r" or "R":
+        print("It is an Alphabet")
+    elif alphabet == "s" or "S":
+        print("It is an Alphabet")
+    elif alphabet == "t" or "T":
+        print("It is an Alphabet")
+    elif alphabet == "u" or "U":
+        print("It is an Alphabet")
+    elif alphabet == "v" or "V":
+        print("It is an Alphabet")
+    elif alphabet == "w" or "W":
+        print("It is an Alphabet")
+    elif alphabet == "x" or "X":
+        print("It is an Alphabet")
+    elif alphabet == "y" or "Y":
+        print("It is an Alphabet")
+    elif alphabet == "z" or "Z":
+        print("It is an Alphabet")
+    elif alphabet == 1 or 2 or 3 or 4 or 5 or 6 or 6 or 7 or 8 or 9 or 10:
+        print("This is not an alphabet")
 else:
-    print("This is not an alphabet")
+    print("Please enter a single word.")
