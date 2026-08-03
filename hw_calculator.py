7937d1f92c6c1bc7abcc2cf4fdbc5375fd9181ce
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-08-03 16:01:08 +0800

    hw_calculator.py

---
+++ hw_calculator.py
@@ -0,0 +1,10 @@
+print("Calculating Power")
+print("Write the number of the suare root and by what power you want to multiply it by.")
+print("X = The number")
+print("Y = The power")
+print("Only enter numbers or it wont work")
+
+x = int(input("Enter the number here: "))
+y = int(input("Enter the power here: "))
+
+print(x**y)
