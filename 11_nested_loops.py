a7862478369b4dffd83f77a267a69db7df63de4f
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-08-05 18:22:33 +0800

    11

---
+++ 11_nested_loops.py
@@ -0,0 +1,7 @@
+# nested loops in python
+fruits = ["apple", "banana", "cherry", "dragonfruit"]
+veggies = ["potato", "onion", "garlic"]
+
+for fruit in  fruits:
+    for veggie in veggies:
+        print(fruit, veggie)
