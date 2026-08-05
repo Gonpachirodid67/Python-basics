f62e54272f10a552f694f25b55728ff4ad05d26e
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-08-05 17:38:17 +0800

    hw_calculator.py

--- hw_calculator.py
+++ hw_calculator.py
@@ -4,38 +4,46 @@ bruh = True
 
 while(bruh):
     try:
-        sui = (input("What is your question today (square roots/addition/subtraction/multiplication/division/remainder): ").lower())
+        sui = (input("What is your question today (Square Roots/Addition/Subtraction/Multiplication/Division/Area/Perimeter): ").lower())
         if sui == "square roots":
             print("Write the number of the square root and by what power you want to multiply it by.")
             print("Only enter numbers or it wont work")
             a = int(input("Enter the number here: "))
             b = int(input("Enter the power here: "))
-            print(a**b)
+            print("Square Root of the number: ", a**b)
             bruh = False
         elif sui == "addition":
             print("Enter the numbers whose sum you want to find out.")
             c = int(input("Enter number 1: "))
             d = int(input("Enter number 2: "))
-            print(c + d)
+            print("Sum of the numbers: ", c + d)
             bruh = False
         elif sui == "subtraction":
             print("Enter the numbers whose difference you want to know.")
             e = int(input("Enter number 1: "))
             f = int(input("Enter number 2: "))
-            print(e - f)
+            print("Difference of the numbers: ", e - f)
             bruh = False
         elif sui == "multiplication":
             print("Enter the numbers whose product you want to know.")
             g = int(input("Enter number 1: "))
             h = int(input("Enter number 2: "))
-            print(g * h)
+            print("Product of the numbers: ", g * h)
             bruh = False
         elif sui == "division":
             print("Enter the numbers whose quotient")
             i = int(input("Enter the numerator here: "))
             j = int(input("Enter the denominator here: "))
-            print(i/j)
+            print("Quotient of the numbers", i/j)
             bruh = False
+        elif sui == "area":
+            length = int(input("Enter length: "))
+            width = int(input("Enter width: "))
+            print("Area: ", length * width)
+        elif sui == "perimeter":
+            length = int(input("Enter length: "))
+            width = int(input("Enter width: "))
+            print("Perimeter: ", 2 * (length + width))
 
         print("Only Yes or No question.")
         more = (input("Do you have more questions?(Yes/No) ").lower())
