aabecff4d88a2859c2fb4a712b3c4bee0033314f
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-08-20 17:49:15 +0800

    ageexcept

---
+++ hw_age_except.py
@@ -0,0 +1,14 @@
+try:
+    age = int(input("Enter your age: "))
+
+    if age < 0:
+        print("Value Error: Age cannot be negative.")
+
+    elif age % 2 == 0:
+        print("Your age is even.")
+
+    else:
+        print("Your age is odd.")
+
+except ValueError:
+    print("Value Error: Please enter a whole number.")
