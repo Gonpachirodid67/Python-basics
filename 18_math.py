08cecc215a39749187e67a466f69f75d1739a6e0
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-08-21 18:22:03 +0800

    18

---
+++ 18_math.py
@@ -0,0 +1,32 @@
+import math
+
+# find the square root of a number
+print(math.sqrt(64))
+
+# find the power of a number
+print(math.pow(2, 3))
+
+# floor
+print(math.floor(7.92))
+print(math.floor(5.36))
+
+# ceil
+print(math.ceil(7.92))
+print(math.ceil(5.36))
+
+# trunc
+print(math.trunc(78.1253))
+
+# factorial
+print(math.factorial(5))
+print(math.factorial(10))
+
+# isnan => not a number
+print(math.isnan(15))
+print(math.isnan(float("naan")))
+
+# gcd - greatest common divisor (HCF)
+print(math.gcd(48, 96))
+
+# lcm - lowest common multiple
+print(math.lcm(12, 36, 48))
--- 18_random.py
+++ 18_random.py
@@ -9,3 +9,18 @@ print(random.random())
 # selecting a random 'char' from a string
 word = "Computer"
 print(random.choice(word))
+
+# generate a random float between 2 numbers
+print(random.uniform(20, 50))
+
+# generate a random integer in a range with step
+print(random.randrange(0, 20, 3))
+
+# random shuffle of items
+fruits = ["apple", "orange", "guava", "watermelon"]
+random.shuffle(fruits)
+print(fruits)
+
+# creates a random list from an existing one
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(random.sample(letters, k=2))
