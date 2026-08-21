f9a1b324e93f2353d424a86ea30deb729a1beee9
Gonpachirodid67 <shlokbarai01@gmail.com>
2026-08-21 17:45:39 +0800

    random

---
+++ 18_random.py
@@ -0,0 +1,11 @@
+import random 
+
+# generate a random  integer
+print(random.randint(0, 10))
+
+# generate a random integer between 0 and 1
+print(random.random())
+
+# selecting a random 'char' from a string
+word = "Computer"
+print(random.choice(word))
